f0b655c28454bda10fb25db5cfc10a8b5739663a
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2021-12-27 11:21:23 +0100

    Update population.c

--- pset1/population.c
+++ pset1/population.c
@@ -1,9 +1,11 @@
+//We have a population of n llamas. Each year, n / 3 new llamas are born, and n / 4 llamas pass away. How many years are needed to reach a population of m llamas?
+
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    // TODO: Prompt for start size
+    // TODO: Ask the user for the initial population (that must be above 9)
     int start_pop;
     do
     {
@@ -11,7 +13,7 @@ int main(void)
     }
     while (start_pop < 9);
 
-    // TODO: Prompt for end size
+    // TODO: Ask for the ending population (in this case, higher than initial pop)
     int ending_pop;
     do
     {
@@ -19,7 +21,7 @@ int main(void)
     }
     while (ending_pop < start_pop);
     
-    // TODO: Calculate number of years until we reach threshold
+    // TODO: Calculate number of years until we reach objective
     int y = 0;
     while (ending_pop > start_pop)
     {
