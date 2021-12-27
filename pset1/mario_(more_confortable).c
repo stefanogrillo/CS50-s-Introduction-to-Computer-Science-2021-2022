6043a99ad36fe79598736b8e5227244acd9bce24
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2021-12-27 11:27:03 +0100

    Create mario_(more_confortable).c

---
+++ pset1/mario_(more_confortable).c
@@ -0,0 +1,14 @@
+// Create a pyramid printing "#" in two separated triangles, like the pyramids with the hole in the game
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Ask for pyramid size
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+}
