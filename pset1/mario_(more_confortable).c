9aaa8d63115be5650ece4992e8b0106f0255b680
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2021-12-27 12:51:27 +0100

    Update mario_(more_confortable).c

--- pset1/mario_(more_confortable).c
+++ pset1/mario_(more_confortable).c
@@ -14,11 +14,26 @@ int main(void)
     // Size must be between 1 and 8 included
     while (height < 1 || height > 8);
     
-     for (int lenght = 0; lenght < height; lenght++)
+    for (int lenght = 0; lenght < height; lenght++)
     {
-         for (int hash = 0; hash < lenght + 1; hash++)
+        for (int space = height - lenght - 1; space > 0; space--)
         {
-            // Print "#" from 1 to hash times, one more hash each new row
+            // Print " " from 7 to 0 times per row, one less space each new row (to move the "#" to the right)
+            printf(" ");
+        }
+
+        for (int hash = 0; hash < lenght + 1; hash++)
+        {
+            // Print "#" from 1 to hash times, one more hash each new row (left triangle of the pyramid)
+            printf("#");
+        }
+
+        // Add a separator between right-skewed pyramid and left skewed pyramid
+        printf("  ");
+        
+        for (int hash = 0; hash < lenght + 1; hash++)
+        {
+            // Print "#" from 1 to hash times, one more hash each new row (same as rows 24 : 28)
             printf("#");
         }
 
