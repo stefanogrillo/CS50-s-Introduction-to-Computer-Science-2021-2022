c1e79bfd398309d8cc69631c4af036d48ba2e477
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2021-12-31 13:58:06 +0100

    Create readability.c

---
+++ pset2/readability.c
@@ -0,0 +1,10 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+// Prototypes
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
