56398c68f645f7ee1d0b175b1c2963597fc4665a
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-07 19:41:40 +0100

    Update plurality.c

--- pset3/plurality.c
+++ pset3/plurality.c
@@ -22,3 +22,58 @@ int candidate_count;
 // Function prototypes
 bool vote(string name);
 void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates: check the number, use argv[i + 1] (because the first i is ./plurality) to set candidates[i] 
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    // Check if voted names == candidates' names; if so, increase their votes
+    for (int i = 0, n = candidate_count; i < n; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    return false;
+}
