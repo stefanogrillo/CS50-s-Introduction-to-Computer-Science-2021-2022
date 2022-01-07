71f4def5555deb60c1592bcb1836dfe9e00967b5
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-07 12:37:59 +0100

    Update plurality.c

--- pset3/plurality.c
+++ pset3/plurality.c
@@ -8,10 +8,17 @@
 // Max number of candidates (constant chosen a priori)
 #define MAX 9
 
-// Candidates have name and vote count (create a 
+// Candidates have name and vote count (create a new data type saving both name and votes)
 typedef struct
 {
     string name;
     int votes;
 }
 candidate;
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
