80c37a80f2a8eb005c780c96dc5986a5c05bd981
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-08 19:37:02 +0100

    Create runoff.c

---
+++ pset3/runoff.c
@@ -0,0 +1,23 @@
+// Similarly to plurality, we want to find the winner of the elections; however, people can set preferences:
+// So if their main choice is eliminated, their second choice is counted
+// Under construction
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max voters and candidates
+#define MAX_VOTERS 100
+#define MAX_CANDIDATES 9
+
+// preferences[i][j] is jth preference for voter i
+int preferences[MAX_VOTERS][MAX_CANDIDATES];
+
+// Candidates have name, vote count, eliminated status
+typedef struct
+{
+    string name;
+    int votes;
+    bool eliminated;
+}
+candidate;
