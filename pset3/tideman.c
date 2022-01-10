52dff5b5ebd6fef10036fdeae45063778522a1af
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-10 21:38:56 +0100

    Update tideman.c

--- pset3/tideman.c
+++ pset3/tideman.c
@@ -54,8 +54,50 @@ int main(int argc, string argv[])
         printf("Maximum number of candidates is %i\n", MAX);
         return 2;
     }
-    // Now populate it with names in a new variable
+    // Now populate it with names in the array previously created
     for (int i = 0; i < candidate_count; i++)
     {
         candidates[i] = argv[i + 1];
     }
+
+        // Clear graph of locked in pairs
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            locked[i][j] = false;
+        }
+    }
+
+    pair_count = 0;
+    int voter_count = get_int("Number of voters: ");
+
+    // Query for votes
+    for (int i = 0; i < voter_count; i++)
+    {
+        // ranks[i] is voter's ith preference (j is not needed because we are inside the candidate's loop)
+        int ranks[candidate_count];
+
+        // Query for each rank
+        for (int j = 0; j < candidate_count; j++)
+        {
+            string name = get_string("Rank %i: ", j + 1);
+
+            if (!vote(j, name, ranks))
+            {
+                printf("Invalid vote.\n");
+                return 3;
+            }
+        }
+        
+        record_preferences(ranks);
+
+        printf("\n");
+    }
+
+    add_pairs();
+    sort_pairs();
+    lock_pairs();
+    print_winner();
+    return 0;
+}
