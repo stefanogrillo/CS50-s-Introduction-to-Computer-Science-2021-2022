ce37b70328fbd06e86254f13201eb95892b98fff
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-17 09:30:33 +0100

    Create volume.c

---
+++ pset4/volume.c
@@ -0,0 +1,17 @@
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
