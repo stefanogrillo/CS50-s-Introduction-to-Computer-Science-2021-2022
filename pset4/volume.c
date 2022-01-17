ef14d9a9837f9693b689022588ee2f19400e9ba4
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-17 09:36:34 +0100

    Update volume.c

--- pset4/volume.c
+++ pset4/volume.c
@@ -15,3 +15,39 @@ int main(int argc, char *argv[])
         printf("Usage: ./volume input.wav output.wav factor\n");
         return 1;
     }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // Select the size of the header, copy the header to output
+    uint8_t header[HEADER_SIZE];
+    fread(header, sizeof(header), 1, input);
+    fwrite(header, sizeof(header), 1, output);
+
+    // Standard size of 2 bytes
+    int16_t buffer;
+    // Read samples from input file with a standard size; change the volume and write samples in &buffer (a pointer)
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+}
