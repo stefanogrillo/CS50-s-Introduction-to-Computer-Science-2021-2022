ddeedd07075906453b50883aaee34eda36ebca78
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-18 20:53:54 +0100

    Update helpers.c

--- pset4/filter_(less)/helpers.c
+++ pset4/filter_(less)/helpers.c
@@ -29,3 +29,36 @@ int limit(int RGB)
     }
     return RGB;
 }
+
+// Convert image to sepia (works with "limit" function from above)
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    // For every row
+    for (int i = 0, n = height; i < n; i++)
+    {
+        // For every RGBTRIPLE (a pixel)
+        for (int j = 0, m = width; j < m; j++)
+        {
+            // Use the Sepia Red Formula
+            int sepiaRed = limit(round(0.393 * image[i][j].rgbtRed
+            + 0.769 * image[i][j].rgbtGreen
+            + 0.189 * image[i][j].rgbtBlue));
+
+            //Use the Sepia Green Formula
+            int sepiaGreen = limit(round(0.349 * image[i][j].rgbtRed
+            + 0.686 * image[i][j].rgbtGreen
+            + 0.168 * image[i][j].rgbtBlue));
+
+            // Use the Sepia Blue Formula
+            int sepiaBlue = limit(round(0.272 * image[i][j].rgbtRed
+            + 0.534 * image[i][j].rgbtGreen
+            + 0.131 * image[i][j].rgbtBlue));
+
+            // Apply new shades
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
+    return;
+}
