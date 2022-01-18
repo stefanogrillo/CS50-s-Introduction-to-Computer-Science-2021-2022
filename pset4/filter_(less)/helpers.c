a6506f77adfffff87ca2c84b138441d3f6a91835
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-18 20:06:22 +0100

    Create helpers.c

---
+++ pset4/filter_(less)/helpers.c
@@ -0,0 +1,21 @@
+// Implementation of the 4 filters 
+
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0, n = height; i < n; i++)
+    {
+        for (int j = 0, m = width; j < m; j++)
+        {
+            // Find the average value of Red, Green and Blue
+            int avg = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue)/ 3.00);
+
+            // Set the new values
+            image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = avg;
+        }
+    }
+    return;
+}
