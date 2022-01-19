d4d31bde3640a8a7e9620abc4e18fa05eeb59b0a
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-19 10:45:16 +0100

    Update helpers.c

--- pset4/filter_(less)/helpers.c
+++ pset4/filter_(less)/helpers.c
@@ -6,21 +6,23 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // For every column
     for (int i = 0, n = height; i < n; i++)
     {
+        // For every row (which, combined to the above, means: for every pixel)
         for (int j = 0, m = width; j < m; j++)
         {
             // Find the average value of Red, Green and Blue
             int avg = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.00);
 
-            // Set the new values
+            // Set the new values (when RGB values are equal, we have a shade of b/w)
             image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = avg;
         }
     }
     return;
 }
 
-// Set limit to RGB 255
+// Set upper limit to RGB 255 (the maximum RGB value possible)
 int limit(int RGB)
 {
     if (RGB > 255)
@@ -33,10 +35,8 @@ int limit(int RGB)
 // Convert image to sepia (works with "limit" function from above)
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    // For every row
     for (int i = 0, n = height; i < n; i++)
     {
-        // For every RGBTRIPLE (a pixel)
         for (int j = 0, m = width; j < m; j++)
         {
             // Use the Sepia Red Formula
@@ -68,15 +68,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0, n = height; i < n; i++)
     {
+        // Consider that you have to stop halfway, or the result will be a central reflection of the Right side (R-R)
         for (int j = 0, m = width / 2; j < m; j++)
         {
-            // Create a temporary value to save RGB values on the Left
+            // Create a temporary value to save RGB values from the Left side (till center) (L)
             RGBTRIPLE temp = image[i][j];
 
-            // Copy and paste Right-to-Left
+            // Copy and paste Right-to-Left (till center) (R-L)
             image[i][j] = image[i][width - j - 1];
 
-            // Copy and paste Left-to-Right
+            // Copy and paste Left-to-Right (till center) (L-R)
             image[i][width - j - 1] = temp;
         }
     }
@@ -86,7 +87,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Create an operative copy to read from
+    // Create an operative copy to read from later on
     RGBTRIPLE copy[height][width];
     for (int i = 0, n = height; i < n; i++)
     {
@@ -96,19 +97,21 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
-    // Read from copy[i][j] and write to image[i][j]
+    // Read from copy[i][j] (to avoid averaging the same square pixels more than once) and write to image[i][j]
     for (int i = 0, n = height; i < n; i++)
     {
         for (int j = 0, m = width; j < m; j++)
         {
-            // Initialize counters (needed to make the averages)
+            // Initialize counters (needed to make the averages of RGB values)
             float sumRed = 0, sumGreen = 0, sumBlue = 0;
             int counter = 0;
             
             // To Blur the image, we must average the RGB values of local pixels; we do so considering a square around the pixel[i][j]
             // Loop for the surrounding pixels (the square around the pixel[i][j]: i && i +/- 1; j && j +/- 1)
+            // (i && i +/- 1) i.e. surrounding columns
             for (int k = (i - 1); k < (i + 2); k++)
             {
+                // (j && j +/- 1) i.e. surrounding rows
                 for (int l = (j - 1); l < (j + 2); l++)
                 {
                     // If pixel is out of the column
@@ -123,14 +126,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         continue;
                     }
 
-                    // Otherwise
+                    // Keep track for each move
                     sumRed += copy[k][l].rgbtRed;
                     sumGreen += copy[k][l].rgbtGreen;
                     sumBlue += copy[k][l].rgbtBlue;
                     counter++;
                 }
             }
-            // Calculate the average value per color, then blur
+            // Calculate the average value per color (which means: blur the image)
             image[i][j].rgbtRed = round(sumRed / counter);
             image[i][j].rgbtGreen = round(sumGreen / counter);
             image[i][j].rgbtBlue = round(sumBlue / counter);
