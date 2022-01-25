2f9cabaeb71b789e4c9a31d4a7789b982e854cdc
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-25 17:45:03 +0100

    Update inheritance.c

--- pset5/inheritance.c
+++ pset5/inheritance.c
@@ -80,3 +80,76 @@ person *create_family(int generations)
     // Return newly created person
     return p;
 }
+
+// Free `p` and all ancestors of `p`.
+void free_family(person *p)
+{
+    // Handle base case
+    if (p == NULL)
+    {
+        return;
+    }
+
+    // Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
+
+    // Free child
+    free(p);
+}
+
+// Print each family member and their alleles.
+void print_family(person *p, int generation)
+{
+    // Handle base case
+    if (p == NULL)
+    {
+        return;
+    }
+
+    // Print indentation
+    for (int i = 0; i < generation * INDENT_LENGTH; i++)
+    {
+        printf(" ");
+    }
+
+    // Print person
+    if (generation == 0)
+    {
+        printf("Child (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+    }
+    else if (generation == 1)
+    {
+        printf("Parent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+    }
+    else
+    {
+        for (int i = 0; i < generation - 2; i++)
+        {
+            printf("Great-");
+        }
+        printf("Grandparent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+    }
+
+    // Print parents of current generation
+    print_family(p->parents[0], generation + 1);
+    print_family(p->parents[1], generation + 1);
+}
+
+// Randomly chooses a blood type allele.
+char random_allele()
+{
+    int r = rand() % 3;
+    if (r == 0)
+    {
+        return 'A';
+    }
+    else if (r == 1)
+    {
+        return 'B';
+    }
+    else
+    {
+        return 'O';
+    }
+}
