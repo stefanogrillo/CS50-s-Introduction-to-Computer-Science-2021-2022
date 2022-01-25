d606f308b6cf92181986fe5c8a1125862946edd3
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-25 15:27:10 +0100

    Update inheritance.c

--- pset5/inheritance.c
+++ pset5/inheritance.c
@@ -4,3 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+// Each person has two parents and two alleles
+typedef struct person
+{
+    struct person *parents[2];
+    char alleles[2];
+}
+person;
