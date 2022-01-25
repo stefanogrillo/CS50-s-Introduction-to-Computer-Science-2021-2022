3f9f93110ae8578437ead9a52f5d586506afdbe3
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-25 16:39:00 +0100

    Update inheritance.c

--- pset5/inheritance.c
+++ pset5/inheritance.c
@@ -37,3 +37,46 @@ int main(void)
     // Free memory
     free_family(p);
 }
+
+// Create a new individual with `generations`
+person *create_family(int generations)
+{
+    // Allocate memory for new person
+    person *p = malloc(sizeof(person));
+    if (p == NULL)
+    {
+        return NULL;
+    }
+
+    // If there are still generations left to be created
+    if (generations > 1)
+    {
+        // Create two new parents for current person by recursively calling create_family
+        person *parent0 = create_family(generations - 1);
+        person *parent1 = create_family(generations - 1);
+
+        // Set parent pointers for current person
+        p->parents[0] = parent0;
+        p->parents[1] = parent1;
+
+        // Randomly assign current person's alleles based on the alleles of their parents
+        p->alleles[0] = p->parents[0]->alleles[rand() % 2];
+        p->alleles[1] = p->parents[1]->alleles[rand() % 2];
+        // [rand() % 2] Means that you simply randomly choose one allele (a number) out of parent's two
+    }
+
+    // If there are no generations left to be created
+    else
+    {
+        // Set parent pointers to NULL
+        p->parents[0] = NULL;
+        p->parents[1] = NULL;
+
+        // Randomly assign alleles
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
+    }
+
+    // Return newly created person
+    return p;
+}
