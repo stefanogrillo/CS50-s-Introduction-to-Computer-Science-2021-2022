36aa8974910f61b72663aea3e99222c9bd7023dd
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-28 15:15:00 +0100

    Update dictionary.c

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -19,3 +19,32 @@ typedef struct node
     struct node *next;
 }
 node;
+
+// Number of buckets in hash table (26^3)
+const unsigned int N = 17576;
+
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // Find the Hash Value for Word
+    int number = hash(word);
+    // Go to that place in the Table
+    node *cursor = table[number];
+
+    // Loop the cursor till end of Linked List (traverse the LL)
+    while (cursor != NULL)
+    {
+        // The function "strcasecmp" is case insensitive
+        // There is no need for "tolower(word[i])" inside a "for (int i...)" loop
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+        // Go to the next node
+        cursor = cursor->next;
+    }
+    return false;
+}
