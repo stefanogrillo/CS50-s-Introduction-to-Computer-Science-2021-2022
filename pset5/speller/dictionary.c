ff31d8e7fdac5fbe945de371f5086d6e24811f2c
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-01-28 14:23:55 +0100

    Create dictionary.c

---
+++ pset5/speller/dictionary.c
@@ -0,0 +1,21 @@
+// Implements a dictionary's functionality
+
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+// Create a counter for only-positive values   
+unsigned int imported_words = 0;
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
