18eff1e4f30280c553ddf5fcd3af184e5709304b
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-02-06 12:33:44 +0100

    Update recursion-factorial.c

--- random/recursion-factorial.c
+++ random/recursion-factorial.c
@@ -1,2 +1,38 @@
-// Create a factorial function using Recursion
+// Create a factorial function using Recursion (for both positive and negative values)
 
+#include<stdio.h>
+
+
+int factorialfunction(int n);
+
+int main()
+{
+    int number;
+  
+    // Get user's input
+    printf("Enter an integer: ");
+    // Retrieve the input, store it in "number" 
+    scanf("%d", &number);
+
+    // Print the result
+    printf("Factorial of %d is %d\n", number, factorialfunction(number));
+    return 0;
+}
+
+int factorialfunction(int n)
+{
+    // If the number is positive
+    if (n > 1)
+    {
+        return n * factorialfunction(n - 1);
+    }
+    // If the number is negative
+    if (n < 0)
+    {
+        return n * factorialfunction(n + 1);
+    }
+    else
+    {
+        return 1;
+    }
+}
