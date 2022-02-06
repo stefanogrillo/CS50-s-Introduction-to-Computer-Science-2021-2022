1dfc8004c07d11401fb32202845966b70bd4a793
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-02-06 15:06:09 +0100

    Update recursion-fibonacci.c

--- random/recursion-fibonacci.c
+++ random/recursion-fibonacci.c
@@ -3,3 +3,32 @@
 #include <stdio.h>
 
 int fibo(int n);
+
+int main(void)
+{
+    int number;
+    // Ask an input to the user
+    printf("Enter Fibonacci serie's length: ");
+    scanf("%d", &number);
+    
+    // Print as many numbers as required by the user
+    for(int i = 0; i < number; i++)
+    {
+        printf("%d ", fibo(i));
+    }
+    printf("\n");
+}
+
+int fibo(int n)
+{
+    // Recursion is not applicable for the first two numbers of the fibonacci series (n = 0, n = 1)
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    // But here apply recursion
+    else
+    {
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
