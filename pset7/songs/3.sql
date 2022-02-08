41e376e5489c3f3ab23a95d8fb553a18ab1328cf
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-02-08 01:21:09 +0100

    Update 3.sql

--- pset7/songs/3.sql
+++ pset7/songs/3.sql
@@ -1,3 +1,3 @@
--- SQL query to list the names of the top 5 longest songs, in descending order of length --
+-- SQL query to list the names of the top 5 longest songs, in descending order of length 
 
 SELECT name FROM songs ORDER BY duration_ms DESC LIMIT 5;
