8fbc0b6e84b765b295f9e67bd2a74f6e6d0b4e99
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-02-08 01:24:25 +0100

    Create log.sql

---
+++ pset7/fiftyville/log.sql
@@ -0,0 +1,4 @@
+-- Keep a log of any SQL queries you execute as you solve the mystery
+
+-- Open the SQL prompt: sqlite3 fiftyville.db
+-- The theft took place on July 28, 2021 and it took place on Humphrey Street
