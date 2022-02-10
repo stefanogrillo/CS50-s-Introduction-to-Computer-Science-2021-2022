57483f77ef07b976f2c0638d21e2df245e21b871
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-02-10 17:25:50 +0100

    Create .13.sql

---
+++ pset7/movies/.13.sql
@@ -0,0 +1,6 @@
+-- SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred
+
+SELECT name FROM people WHERE name != "Kevin Bacon" AND id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = "1958")));
+
+-- result: 185;
+-- SELECT COUNT(name) FROM people WHERE name != "Kevin Bacon" AND id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Kevin Bacon" AND birth = "1958")));
