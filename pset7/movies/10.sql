04e2858fbce22c556611f9b50dda0e5ec7662dda
Stefano Grillo <96014594+stefanogrillo@users.noreply.github.com>
2022-02-10 17:10:39 +0100

    Create 10.sql

---
+++ pset7/movies/10.sql
@@ -0,0 +1,6 @@
+-- SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0
+
+SELECT name FROM people WHERE id IN (SELECT DISTINCT directors.person_id FROM directors JOIN movies ON movies.id = directors.movie_id WHERE id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0));
+
+-- result: 2236;
+-- SELECT COUNT(name) FROM people WHERE id IN (SELECT DISTINCT directors.person_id FROM directors JOIN movies ON movies.id = directors.movie_id WHERE id IN (SELECT movie_id FROM ratings WHERE rating >= 9.0));
